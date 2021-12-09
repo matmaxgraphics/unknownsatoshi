a9cf172ca98c50f6993dda96c45363c9c03cb68a
Mateen <gbadamosimateen@gmail.com>
2021-12-09 05:45:18 +0100

    created load more button for blog page

--- unknownsatoshi/static/js/pagination.js
+++ unknownsatoshi/static/js/pagination.js
@@ -1,26 +1,56 @@
-// $(".articleCard").slice(0, 3).show()
-// 	$(".articlePageBtn").on("click", function(){
-// 		$(".articleCard:hidden").slice(0, 3).slideDown()
-// 		if($(".articleCard:hidden").length == 0){
-// 			$(".articlePageBtn").fadeOut('slow')
-// 		}
-// })
+    const articleButton = document.querySelector('.articleBtn');
+    let articleItems = 3;
+    articleButton.addEventListener('click', (e) => {
+        const articleList = [...document.querySelectorAll('.blog-post__section #articleCard')];
+        for (let i = articleItems; i < articleItems + 3; i++) {
+            if (articleList[i]) {
+                articleList[i].style.display = 'block';
+            }
+        }
+        articleItems += 3;
 
-// $(".newsCard").slice(0, 3).show()
-// $(".newsPageBtn").on("click", function(){
-//     $(".newsCard:hidden").slice(0, 3).slideDown()
-//     if($(".newsCard:hidden").length == 0){
-//         $(".newsPageBtn").fadeOut('slow')
-//     }
-// })
+        // Load more button will be hidden after list fully loaded
+        if (articleItems >= articleList.length) {
+            event.target.style.display = 'none';
+        }
+    })
 
-// $(".storyCard").slice(0, 3).show()
-// $(".storyPageBtn").on("click", function(){
-//     $(".storyCard:hidden").slice(0, 3).slideDown()
-//     if($(".storyCard:hidden").length == 0){
-//         $(".storyPageBtn").fadeOut('slow')
-//     }
-// })
+
+    /*===========DIVIDER=============*/
+    const newsButton = document.querySelector('.newsBtn');
+    let newsItems = 3;
+    newsButton.addEventListener('click', (e) => {
+        const newsList = [...document.querySelectorAll('.blog-post__section #newsCard')];
+        for (let i = newsItems; i < newsItems + 3; i++) {
+            if (newsList[i]) {
+                newsList[i].style.display = 'block';
+            }
+        }
+        newsItems += 3;
+
+        // Load more button will be hidden after list fully loaded
+        if (newsItems >= newsList.length) {
+            event.target.style.display = 'none';
+        }
+    })
+
+/*===========DIVIDER=============*/
+const storyButton = document.querySelector('.storyBtn');
+let storyItems = 3;
+storyButton.addEventListener('click', (e) => {
+    const storyList = [...document.querySelectorAll('.blog-post__section #storyCard')];
+    for (let i = storyItems; i < storyItems + 3; i++) {
+        if (storyList[i]) {
+            storyList[i].style.display = 'block';
+        }
+    }
+    storyItems += 3;
+
+    // Load more button will be hidden after list fully loaded
+    if (storyItems >= storyList.length) {
+        event.target.style.display = 'none';
+    }
+})
 
 /*==PAGINATION SCRIPT FOR COURSE PAGE==*/
 $(".course-container__item").slice(0, 3).show()
