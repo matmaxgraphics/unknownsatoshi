a2477d55de9d79099bed86c7921c658470734c35
Mateen.io <gbadamosimateen@gmail.com>
2022-10-16 15:38:30 +0100

    added like and commenting features

--- unknownsatoshi/static/js/script.js
+++ unknownsatoshi/static/js/script.js
@@ -1,18 +1,22 @@
 //Owl Carousel JS control
 
-
-
 //Navbar Toggling
 function classToggle() {
-    const navs = document.querySelectorAll('.navbar__Items')
+  const navs = document.querySelectorAll('.navbar__Items');
     
-    navs.forEach(nav => nav.classList.toggle('navbar__ToggleShow'));
-  }
+  navs.forEach(nav => nav.classList.toggle('navbar__ToggleShow'));
+}
   
-  document.querySelector('.navbar__Link-toggle')
-    .addEventListener('click', classToggle);
+document.querySelector('.navbar__Link-toggle').addEventListener('click', classToggle);
 
 
-    
+//like toggle function
+const likeBtn = document.querySelector('.like-btn');
+const likeIcon = document.querySelector('.ri-heart-line');
+const likeNumber = document.querySelector('.action-number__like');
 
 
+likeBtn.addEventListener('click', function(){
+  likeIcon.classList.toggle("ri-heart-line");
+  likeIcon.classList.add("ri-heart-fill");
+})
